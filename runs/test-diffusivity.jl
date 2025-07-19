2566310bdfe909e8594b95deac33864d64369b95
Johan Hidding <j.hidding@esciencecenter.nl>
2025-07-19 10:34:44 +0200

    add diagram explaining active layer; initiate new section on parameter choices in transport model

---
+++ runs/test-diffusivity.jl
@@ -0,0 +1,72 @@
+# ~/~ begin <<md/paper.md#runs/test-diffusivity.jl>>[init]
+#| file: runs/test-diffusivity.jl
+using CarboKitten
+using CarboKitten.Components: WaterDepth
+using CarboKitten.Components.Common
+using ModuleMixins
+
+@compose module CustomProduction
+@mixin Tag, ActiveLayer
+
+@kwdef struct Input <: AbstractInput
+    production    # a function of (facies, wd)
+end
+
+function initial_state(input::AbstractInput)
+    sediment_height = zeros(Height, input.box.grid_size...)
+    sediment_buffer = zeros(Float64, input.sediment_buffer_size, n_facies(input), input.box.grid_size...)
+    active_layer = zeros(Amount, n_facies(input), input.box.grid_size...)
+
+    state = State(
+        step=0, sediment_height=sediment_height,
+        sediment_buffer=sediment_buffer,
+        active_layer = active_layer)
+
+    return state
+end
+
+function step!(input::Input)
+    disintegrate! = ActiveLayer.disintegrator(input)
+    transport! = ActiveLayer.transporter(input)
+    local_water_depth = water_depth(input)
+    x, y = box_axes(input.box)
+    na = [CartesianIndex()]
+    produce(_, wd) = input.production.(x[:,na], y[na,:], wd)[na,:,:]
+    pf = precipitation_factor(input)
+
+    function (state::State)
+        if mod(state.step, input.ca_interval) == 0
+            step_ca!(state)
+        end
+
+        wd = local_water_depth(state)
+        p = produce(state, wd)
+        d = disintegrate!(state)
+
+        state.active_layer .+= p
+        state.active_layer .+= d
+        transport!(state)
+
+        deposit = pf .* state.active_layer
+        push_sediment!(state.sediment_buffer, deposit ./ input.depositional_resolution .|> NoUnits)
+        state.active_layer .-= deposit
+        state.sediment_height .+= sum(deposit; dims=1)[1,:,:]
+        state.step += 1
+
+        return Frame(
+            production = p,
+            disintegration = d,
+            deposition = deposit)
+    end
+end
+
+
+end
+
+module TestDiffusivity
+
+function main()
+end
+
+end
+# ~/~ end
