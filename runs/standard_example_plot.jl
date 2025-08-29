d81ce11d03d414d72231bc6f74a33f9377dc2851
Emilia Jarochowska <e.b.jarochowska@uu.nl>
2025-08-29 22:21:29 +0200

    add add_panel_labels! function

--- runs/standard_example_plot.jl
+++ runs/standard_example_plot.jl
@@ -8,8 +8,39 @@ module StandardExamplePlot
 using CairoMakie
 using CarboKitten.Visualization: summary_plot
 
+function add_panel_labels!(fig::Figure; 
+                          labels::Vector{String}, 
+                          fontsize::Int64,
+                          offset::Tuple{Int64, Int64})
+    
+    panel_positions = [
+        (1, 1), # a 
+        (1, 3), # b 
+        (2, 3), # c 
+        (3, 1), # d 
+        (3, 2), # e 
+        (3, 3)  # f 
+    ]
+    
+    for (i, (row, col)) in enumerate(panel_positions)
+        if i <= length(labels)   
+            Label(fig[row, col], labels[i], 
+                  tellwidth=false, 
+                  tellheight=false,
+                  halign=:left,
+                  valign=:top,
+                  padding=(offset[1], offset[1], -offset[2], offset[1]),
+                  fontsize=fontsize)
+        end
+    end
+    
+    return fig
+end
+
+
 function main()
     fig = summary_plot("data/alcap-example.h5")
+    add_panel_labels!(fig, labels = ["a", "b", "c", "d", "e"], fontsize = 18, offset = (5,-3))
     save("md/fig/summary-plot.png", fig)
 end
 end
