61dc510dc0a00db9c37a3b1ce1878583896c1e24
Johan Hidding <j.hidding@esciencecenter.nl>
2025-09-11 14:57:01 +0200

    fix the citation in caption problem with the ugliest of ugliness

--- pandoc/wide_figures.lua
+++ pandoc/wide_figures.lua
@@ -9,12 +9,19 @@ end
 function Figure(el)
     cls = make_set(el.content[1].content[1].classes)
     if cls.wide then
-        caption_tex = pandoc.write(pandoc.Pandoc{ table.unpack(el.caption.long) }, "latex")
-        return pandoc.RawBlock("latex", "\\begin{figure*}\n" ..
-            "\\includegraphics[width=\\textwidth]{" .. el.content[1].content[1].src .. "}\n" ..
-            "\\caption{" .. caption_tex .. "}\n" ..
-            "\\label{" .. el.identifier .. "}\n" ..
-            "\\end{figure*}")
+        -- caption_tex = pandoc.write(pandoc.Pandoc{ table.unpack(el.caption.long) }, "latex")
+        table.insert(el.caption.long[1].content, 1, pandoc.RawInline("latex", "\\caption{"))
+        table.insert(el.caption.long[1].content, pandoc.RawInline("latex", "}"))
+        print(el.caption.long)
+        return {
+            pandoc.RawBlock("latex", "\\begin{figure*}\n" ..
+                "\\includegraphics[width=\\textwidth]{" .. el.content[1].content[1].src .. "}\n"),
+            -- caption_tex,
+            table.unpack(el.caption.long),
+            pandoc.RawBlock("latex",
+                "\\label{" .. el.identifier .. "}\n" ..
+                "\\end{figure*}")
+        }
     else
         caption_tex = pandoc.write(pandoc.Pandoc{ table.unpack(el.caption.long) }, "latex")
         return pandoc.RawBlock("latex", "\\begin{figure}\n" ..
